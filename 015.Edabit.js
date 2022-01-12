a871c6c8db0579558559ce76e856e37b9d739d15
Rizah Hrustanović <76018126+rhrustanovic@users.noreply.github.com>
2022-01-12 23:59:31 +0100

    Takes an array of numbers, returns both the minimum and maximum

---
+++ 015.Edabit.js
@@ -0,0 +1,7 @@
+// 015.Takes an array of numbers, returns both the minimum and maximum numbers, in that order.
+
+function minMax(arr) {
+	arr.sort(function(a, b){return(a - b)})
+	return [arr[0], arr[arr.length - 1]]
+	
+}
