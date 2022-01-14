04e9191fce25f8f972931b9241ebf979d6a74dcd
Rizah Hrustanović <76018126+rhrustanovic@users.noreply.github.com>
2022-01-14 23:55:47 +0100

    Calculates no of diff squares in n*n square grid.

---
+++ 017.Edabit.js
@@ -0,0 +1,8 @@
+
+// 017.Create a function that calculates the number of different squares in an n * n square grid.
+
+
+function numberSquares(n) {
+	var num = n*(2*n + 1)*(n + 1)/6
+	return (num)
+}
